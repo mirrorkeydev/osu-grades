1dc79cd71b163f58273df974827dabef3204a99d
mirrorkeydev <mirrorkeydev@gmail.com>
2021-11-24 13:29:45 -0800

    Add all engineering instructors by term

--- instructor-data/scraper.py
+++ instructor-data/scraper.py
@@ -17,18 +17,21 @@ TERMS = [
   "summer2021",
 ]
 
-ROOT = "https://classes.engr.oregonstate.edu/eecs/"
+ROOT = "https://classes.engr.oregonstate.edu/"
 
-f = open("eecs-instructors-by-term.csv", "w")
+DISCIPLINES = ["cbee", "cce", "eecs", "engr", "mime"]
+
+f = open("all-engineering-instructors-by-term.csv", "w")
 
 for term in TERMS:
-  page = requests.get(ROOT + term)
-  soup = bs(page.content, 'html.parser')
-  sections = soup.find_all("li")
-  for section in sections:
-    if (match := re.match(r"(\S+) (\d+H?) (?:\(Section (\d+)\) )?(.+) \((.+)\)", section.text)):
-      code, number, section, coursename, instructor = match.groups()
-      if section == '001' or section == None:
-        f.write(f'{term},{code},{number},"{instructor.strip()}"\n')
+  for discipline in DISCIPLINES:
+    page = requests.get(f"{ROOT}/{discipline}/{term}")
+    soup = bs(page.content, 'html.parser')
+    sections = soup.find_all("li")
+    for section in sections:
+      if (match := re.match(r"(\S+) (\d+H?) (?:\(Section (\d+)\) )?(.+) \((.+)\)", section.text)):
+        code, number, section, coursename, instructor = match.groups()
+        if section == '001' or section == None:
+          f.write(f'{term},{code},{number},"{instructor.strip()}"\n')
 
 f.close()
